1b144ca588b774bf78e152ad6d9e0fa6b086fad6
Morlene Fisher <morlene@impressivemedia.co.uk>
2015-02-04 19:59:12 +0000

    Added twitters profile image Added twitter id's to stored array for linking

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-"use strict"
+"use strict";
 
 var Twit = require('twit');
 var io = require('socket.io')(8080);
 var config = require('./config.js');
 var T = new Twit(config.twitter);
 
-var stream = T.stream('statuses/filter', { track: 'superbowl' });
+var stream = T.stream('statuses/filter', {track: 'superbowl'});
 
 stream.on('tweet', function (tweet) {
-  console.log(tweet.user.name);
-  io.emit('tweet', tweet);
-  console.log(tweet);
+    console.log(tweet.user.name);
+    console.log(tweet.text);
+    //console.log(tweet.user.location);
+
+    io.emit('tweet', tweet);
+    //console.log(tweet);
 });
 
 io.sockets.on('connection', function (socket) {
-  console.log("Client connected");
+    console.log("Client connected");
 //  socket.on('message', function () { });
-  socket.on('disconnect', function () {
-    console.log('Client disconnected');
-  });
+    socket.on('disconnect', function () {
+        console.log('Client disconnected');
+    });
 });
